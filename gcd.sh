a6f319d33e5ab5fcf6981bb36506eb9efa1af7eb
AN <a2324an@aiit.ac.jp>
2023-07-30 02:44:34 +0900

    Add more test case.

--- gcd.sh
+++ gcd.sh
@@ -21,9 +21,9 @@ if [ $# -ne 2 ]; then
 fi
 
 # Check number
-is_number() {
+is_natural_number() {
     if ! [[ $1 =~ ^[0-9]+$ ]]; then
-        echo "Error: $1 is not a number."
+        echo "Error: $1 is not a natural number."
         exit 1
     fi
 
@@ -35,8 +35,8 @@ is_number() {
 }
 
 # Check two args.
-is_number $1
-is_number $2
+is_natural_number $1
+is_natural_number $2
 
 # Execute gcd
 gcd $1 $2
--- gcd_test.sh
+++ gcd_test.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 
+something_error=0
+
 test_gcd() {
     local val
     val=$(./gcd.sh $1 $2)
 
     if [ "$val" -ne "$3" ]; then
-        echo "Test failed: gcd of $1 and $2, expected $3, got $val"
-        exit 1
+        echo "Test failed(${BASH_LINENO[0]}): gcd of $1 and $2, expected $3, got $val"
+        something_error=1
     fi
 }
 
@@ -15,8 +17,8 @@ test_error() {
     expected=$3
 
     if [[ $output != $expected ]]; then
-        echo "Test failed: expected \"$expected\", got \"$output\""
-        exit 1
+        echo "Test failed(${BASH_LINENO[0]}): expected \"$expected\", got \"$output\""
+        something_error=1
     fi
 }
 
@@ -24,12 +26,27 @@ test_error() {
 # 正常系テスト
 test_gcd 36 24 12
 test_gcd 48 18 6
+test_gcd 1 1 1
+test_gcd 3 3 3
 test_gcd 101 103 1 # 互いに素の数
 
 # 異常系テスト
-test_error 36 -24 "Error: -24 is not a number." # 負の数
-test_error 36 0 "Error: 0 is a zero." # ゼロ
-test_error 36 "" "Usage: ./gcd.sh num1 num2" # 引数不足
-test_error 36 abc "Error: abc is not a number." # 数値でない
+test_error 24 -24 "Error: -24 is not a natural number." # 負の数
+test_error -1 53 "Error: -1 is not a natural number." # 負の数
+test_error 75 0 "Error: 0 is a zero." # ゼロ
+test_error 0 0 "Error: 0 is a zero." # ゼロ
+test_error 106 "" "Usage: ./gcd.sh num1 num2" # 引数不足
+test_error "" "" "Usage: ./gcd.sh num1 num2" # 引数不足
+test_error "" 123 "Usage: ./gcd.sh num1 num2" # 引数不足
+test_error 123 abc "Error: abc is not a natural number." # 数値でない
+test_error abc 44 "Error: abc is not a natural number." # 数値でない
+test_error 1.0 2.0 "Error: 1.0 is not a natural number." # 自然数ではない
+test_error 3 2.0 "Error: 2.0 is not a natural number." # 自然数ではない
+
+if [ $something_error -ne 0 ]
+then
+ echo "Error"
+ exit $something_error
+fi
 
 echo "全てのテストがクリアされました。"
